ec584ca04eded058cfcb85484b162731977e58b7
Roberto <rozamora173@gmail.com>
2024-04-23 23:40:33 -0600

    Commit

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 
 //Accediendo a un archivo json
 const archive = ".\\persons.json"
+const a_archive = ".\\teams.json"
 
 //Mandando un Texto plano
 app.get("/Mario", (req, res) =>{
     res.status(200).send("Ruta de Mario")
+});
+
+//Mandando un Texto plano
+app.get("/Roberto", (req, res) =>{
+    res.status(200).send("Ruta de Roberto")
 });
 
 //Mandando el agrchivo con objetos json
@@ -17,6 +23,16 @@ app.post("/Mario", (req, res) =>{
     //Para observar el estado de respuesta
     //console.log("Estado de respuesta: ",res.statusCode)
 });
+
+//Mandando el agrchivo con objetos json
+app.post("/Roberto", (req, res) =>{
+    //Accediendo al modulo
+    const persons = require(a_archive);
+    res.status(200).json(teams);
+    //Para observar el estado de respuesta
+    //console.log("Estado de respuesta: ",res.statusCode)
+});
+
 
 app.listen(8000, () => {
     console.log("listening exitoso!!");
