74fa219483724402ce4f293887df3dea0ecbcb89
Brosscom27 <mcordovacsx@gmail.com>
2024-04-23 18:22:59 -0600

    Agregando las dos primeras rutas, el archivo json y el codigo de estado

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express();
+
+//Accediendo a un archivo json
+const archive = ".\\persons.json"
+
+//Mandando un Texto plano
+app.get("/Mario", (req, res) =>{
+    res.send("Ruta de Mario")
+});
+
+//Mandando el agrchivo con objetos json
+app.post("/Mario", (req, res) =>{
+    //Accediendo al modulo
+    const persons = require(archive);
+    res.status(200).json(persons);
+    //Para observar el estado de respuesta
+    //console.log("Estado de respuesta: ",res.statusCode)
+});
+
+app.listen(8000, () => {
+    console.log("listening exitoso!!");
+});
